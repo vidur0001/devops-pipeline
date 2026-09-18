fbf9a056f800c94d1f1884fae60b59a1e37fbfc3
vidur <vidursharma8035@gmail.com>
2026-09-19 00:08:46 +0530

    Fix pipeline: rename test..js to test.js, fix setTimeout/console.log casing, fix workflow YAML

--- test..js
+++
@@ -1,3 +0,0 @@
-console.log("Starting test...");
-SetTimeout(() => { Console.log("Waiting for 3 seconds"); }, 3000);
-console.log("Test complete...");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting test...");
+setTimeout(() => { console.log("Waiting for 3 seconds"); }, 3000);
+console.log("Test complete...");
